b81baece08e6f1416afe993978ca22f0eced066e
조원우 <greenaloud@gmail.com>
2023-04-06 01:37:32 +0900

    bowling3

--- test/bowling_test.exs
+++ test/bowling_test.exs
@@ -7,4 +7,9 @@ defmodule BowlingTest do
     score = Bowling.roll(0, 20)
     assert(score === 0)
   end
+
+  test "Roll 4 20 times" do
+    score = Bowling.roll(4, 20)
+    assert(score == 80)
+  end
 end
