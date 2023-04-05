379215b1c154e77dd22febfa1743ac2e0eb3c1b7
조원우 <greenaloud@gmail.com>
2023-04-06 01:33:05 +0900

    bowling2

--- lib/bowling.ex
+++ lib/bowling.ex
@@ -1,3 +1,6 @@
 defmodule Bowling do
 
+  def roll(_, 0), do: 0
+  def roll(score, roll_count), do: score + roll(score, roll_count - 1)
+
 end
--- test/bowling_test.exs
+++ test/bowling_test.exs
@@ -3,6 +3,8 @@ defmodule BowlingTest do
 
   import Bowling
 
-  test "nothing" do
+  test "Roll 0 20 times" do
+    score = Bowling.roll(0, 20)
+    assert(score === 0)
   end
 end
